2c9932403a7adb4403f98dc78c2c2f9329a6a2d9
franciscoghp <francisco9mil@gmail.com>
2024-03-21 16:26:40 -0400

    commit develop

--- utils/dates.ts
+++
@@ -1,3 +0,0 @@
-export function formatDate(date: string | number | Date) {
-  return new Intl.DateTimeFormat('es-MX', { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'America/Mexico_City' }).format(new Date(date))
-}
